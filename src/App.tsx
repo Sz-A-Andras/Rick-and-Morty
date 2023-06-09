b10e2c40f9e23c57984b15004cae4ad0d7b72292
Sz-A-Andras <sztancsik.andrasi.andras@gmail.com>
2023-06-09 22:11:22 +0200

    data got from the endpoint

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,28 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import CharacterList from "./components/CharacterList";
+import axios from "axios";
+
+interface Character {
+  id: number;
+  name: string;
+  species: string;
+  status: string;
+}
 
 function App() {
-  const characters = [
+  const characters0 = [
     { id: 0, name: "aaa", species: "bbb", status: "ccc" },
     { id: 1, name: "aaa", species: "bbb", status: "ccc" },
     { id: 2, name: "aaa", species: "bbb", status: "ccc" },
   ];
+
+  const [characters, setCharacters] = useState<Character[]>([]);
+
+  useEffect(() => {
+    axios
+      .get("https://rickandmortyapi.com/api/character")
+      .then((res) => setCharacters(res.data.results));
+  }, []);
 
   return <CharacterList characters={characters} />;
 }
