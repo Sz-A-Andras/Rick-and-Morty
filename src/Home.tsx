94925bf01378e9962cb5597ca43645c01f0d8934
Sz-A-Andras <sztancsik.andrasi.andras@gmail.com>
2023-06-11 13:01:13 +0200

    unused imports deleted

--- src/Home.tsx
+++ src/Home.tsx
@@ -3,16 +3,7 @@ import CharacterList from "./components/CharacterList";
 import axios from "axios";
 import { Character } from "./components/CharacterList";
 import NavBar from "./components/NavBar";
-import { Grid, GridItem, Link } from "@chakra-ui/react";
-import {
-  Routes,
-  Route,
-  useNavigate,
-  Router,
-  createSearchParams,
-} from "react-router-dom";
-import Profile from "./Profile";
-import CharacterFilter from "./components/CharacterFilterBySpecies";
+import { Grid, GridItem } from "@chakra-ui/react";
 import CharacterFilterBySpecies from "./components/CharacterFilterBySpecies";
 import CharacterFilterByName from "./components/CharacterFilterByName";
 
@@ -32,32 +23,17 @@ const Home = () => {
         c.name.includes(selectedCharacterByName) &&
         c.species === selectedCharacterBySpecies
     );
-    console.log(selectedCharacterBySpecies);
-  } else if (selectedCharacterBySpecies === null) {
-    visible = characters;
   } else {
     visible = characters.filter((c) =>
       c.name.includes(selectedCharacterByName)
     );
   }
-  const visibleByName =
-    selectedCharacterByName || selectedCharacterBySpecies
-      ? characters.filter(
-          (c) =>
-            c.name.includes(selectedCharacterByName) &&
-            c.species === selectedCharacterBySpecies
-        )
-      : characters;
 
   useEffect(() => {
     axios
       .get("https://rickandmortyapi.com/api/character")
       .then((res) => setCharacters(res.data.results));
   }, []);
-
-  const visibleCharactersBySpecies = selectedCharacterBySpecies
-    ? characters.filter((c) => c.species === selectedCharacterBySpecies)
-    : characters;
 
   return (
     <Grid templateAreas={`"nav" " main"`}>
--- src/components/CharacterFilterBySpecies.tsx
+++ src/components/CharacterFilterBySpecies.tsx
@@ -1,5 +1,4 @@
 import { Box, Select } from "@chakra-ui/react";
-import React from "react";
 
 interface Props {
   onSelectCharacter: (species: string) => void;
