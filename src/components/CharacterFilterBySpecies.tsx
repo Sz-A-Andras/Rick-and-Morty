26488217581879d9943d665e5602478484786dd6
Sz-A-Andras <sztancsik.andrasi.andras@gmail.com>
2023-06-11 16:07:50 +0200

    more species added

--- src/components/CharacterFilterBySpecies.tsx
+++ src/components/CharacterFilterBySpecies.tsx
@@ -12,8 +12,15 @@ const CharacterFilterBySpecies = ({ onSelectCharacter }: Props) => {
         onChange={(event) => onSelectCharacter(event.target.value)}
       >
         <option value="">All characters</option>
+        <option value="Poopybutthole">Poopybutthole</option>
         <option value="Human">Human</option>
         <option value="Alien">Alien</option>
+        <option value="Mythological Creature">Mythological Creature</option>
+        <option value="Disease">Disease</option>
+        <option value="Humanoid">Humanoid</option>
+        <option value="Robot">Robot</option>
+        <option value="Cronenberg">Cronenberg</option>
+        <option value="unknown">unknown</option>
       </Select>
     </Box>
   );
